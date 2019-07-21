4052d0d216d71259b08b8b47ec0f97fe5eaf6fda
Sabik Abtahee <34734363+SabikAbtahee@users.noreply.github.com>
2019-07-21 09:39:26 +0600

    1

--- src/app/profile/services/password-modal.service.ts
+++ src/app/profile/services/password-modal.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { PasswordChangeComponent } from '../components/password-change/password-change.component';
-
 @Injectable({
 	providedIn: 'root'
 })
 export class PasswordModalService {
-	constructor(public dialog: MatDialog) {}
+	constructor(public dialog: MatDialog) {
+		
+		
+	}
 
 	openPasswordChangeModal(width?: string) {
 		const dialogRef = this.dialog.open(PasswordChangeComponent, {
+			
+			minWidth:'450px',
 			width: width ? width : '35vw'
 			// height:'60vh'
 		});
